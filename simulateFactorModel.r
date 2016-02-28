fe93acad7b81c1f9b99e1f7730b44da798f43606
Chloe <chloe@gmail.com>
2016-02-27 19:18:47 -0500

    Back to previous version

--- simulateFactorModel.r
+++ simulateFactorModel.r
@@ -34,38 +34,3 @@ simulateFactorModel = function(n=10, r=2, T=100, fac.err.mean = rep(0, r), fac.e
     return(list(X=obs, Y=fac))
 }
 
-use.simulateFactorModel() {
-  n=6
-  r=2
-  T=100 
-  fac.err.mean = rep(0, r)
-  fac.err.sd = diag(r)
-  Phi.true = list(matrix(c(0.9, 0, 0, 0.6),ncol=r))
-  Y.start = c(0,0)
-  obs.err.mean = rep(0, n)
-  obs.err.sd = diag(n)
-  set.seed(20141221)
-  Lambda.true = matrix( runif(n*r, min=0, max=1) , nrow=n, ncol=r) 
-  fac.mod = simulateFactorModel(n=n, r=r, T=T, fac.err.mean = fac.err.mean, fac.err.sd = fac.err.sd, Phi = Phi.true, Y.start = Y.start, obs.err.mean = obs.err.mean, obs.err.sd = obs.err.sd, Lambda.true)
-  
-  #--- Get factor Y and obs X --- 
-  X = fac.mod$X
-  Y = fac.mod$Y
-  
-  #--- Add column names ---
-  Y.namestrings = paste0("Y", seq(1:r))
-  dimnames(Y) = list(NULL, Y.namestrings)
-  colMeans(Y)
-
-  X.namestrings = paste0("X", seq(1:n))
-  dimnames(X) = list(NULL, X.namestrings)
-  
-  #--- Plot ---
-  par(mfrow=c(2,1))
-  ts.plot(Y, lty=1:r, ylab="Factors") 
-  legend(locator(1), legend=Y.namestrings, lty=1:r)
-
-  ts.plot(X, col=1:n, ylab="Observations")
-  legend(locator(1), legend=colnames(X), col=1:n)
-}
-
